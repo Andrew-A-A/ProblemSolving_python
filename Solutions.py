c603c95971d904c664f92d97f581e3c1b9a30b74
Andrew Adel <andrew.adel.a@outlook.com>
2023-11-04 13:05:55 +0200

    Add "2887. Fill Missing Data" solution

--- Solutions.py
+++ Solutions.py
@@ -54,3 +54,8 @@ def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
 def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
     students['grade'] = students['grade'].astype(int)
     return students
+
+
+def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+    products['quantity'] = products['quantity'].fillna(0)
+    return products
